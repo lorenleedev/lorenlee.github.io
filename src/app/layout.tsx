30bbfd5662bb769b281bdc3b085ab4563c613bc3
Loren Lee <143101256+lorenleedev@users.noreply.github.com>
2025-11-04 16:56:48 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,19 +5,16 @@ export const metadata: Metadata = {
   description: "Loren Lee's personal site",
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-    <head>
-      <link rel={"stylesheet"} href={"/css/font.css"}></link>
-      <link rel={"icon"} href={"/image/favicon/favicon.ico"}></link>
-      <meta name="google-adsense-account" content="ca-pub-7038308567914787">
-    </head>
+      <head>
+        <link rel="stylesheet" href="/css/font.css" />
+        <link rel="icon" href="/image/favicon/favicon.ico" />
+        <meta name="google-adsense-account" content="ca-pub-7038308567914787">
+      </head>
       <body>{children}</body>
     </html>
   )
 }
+
